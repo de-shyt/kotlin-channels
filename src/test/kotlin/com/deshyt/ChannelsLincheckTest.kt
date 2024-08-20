d71168b8183101c8a2a89ffb5144983cf540619c
Daria.Shutina <de.shutina@gmail.com>
2024-08-20 22:05:17 +0200

    add SequentialBuffered1Channel

--- src/test/kotlin/com/deshyt/ChannelsLincheckTest.kt
+++ src/test/kotlin/com/deshyt/ChannelsLincheckTest.kt
@@ -2,11 +2,12 @@
 
 package com.deshyt
 
+import com.deshyt.buffered.BufferedChannel
 import com.deshyt.rendezvous.RendezvousChannel
 import kotlinx.coroutines.CancellableContinuation
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.suspendCancellableCoroutine
-import java.util.*
+import kotlin.collections.ArrayList
 
 
 class RendezvousChannelTest : ChannelTestBase(
@@ -14,42 +15,105 @@ class RendezvousChannelTest : ChannelTestBase(
     sequentialSpecification = SequentialRendezvousChannel::class.java,
 )
 
+class Buffered2ChannelTest : ChannelTestBase(
+    c = BufferedChannel(2),
+    sequentialSpecification = SequentialBuffered2Channel::class.java,
+)
+
+class Buffered1ChannelTest : ChannelTestBase(
+    c = BufferedChannel(1),
+    sequentialSpecification = SequentialBuffered1Channel::class.java,
+)
+
 // Sequential specification for a rendezvous channel
 class SequentialRendezvousChannel {
-    private val senders   = ArrayList<Pair<CancellableContinuation<Unit>, Int>>()
+    private val senders = ArrayList<Pair<CancellableContinuation<Unit>, Int>>()
     private val receivers = ArrayList<CancellableContinuation<Int>>()
 
     suspend fun send(x: Int) {
         if (resumeFirstReceiver(x)) return
         suspendCancellableCoroutine { cont ->
-            senders.add(Pair(cont, x))
+            senders.add(cont to x)
         }
     }
 
-    private fun resumeFirstReceiver(element: Int): Boolean {
+    private fun resumeFirstReceiver(elem: Int): Boolean {
         while (receivers.isNotEmpty()) {
-            val r = receivers.removeAt(0)
-            if (r.resume(element)) return true
+            val r = receivers.removeFirst()
+            if (r.resume(elem)) return true
         }
         return false
     }
 
     suspend fun receive(): Int {
-        val elem = resumeFirstSender()
-        if (elem != null) return elem
-        return suspendCancellableCoroutine { cont ->
-            receivers.add(cont)
-        }
+        return resumeFirstSender()
+            ?: suspendCancellableCoroutine { cont -> receivers.add(cont) }
     }
 
     private fun resumeFirstSender(): Int? {
         while (senders.isNotEmpty()) {
-            val (sender, elem) = senders.removeAt(0)
+            val (sender, elem) = senders.removeFirst()
             if (sender.resume(Unit)) return elem
         }
         return null
     }
 }
+
+// Sequential specification for a buffered channel
+open class SequentialBufferedChannel(
+    private val capacity: Long
+) {
+    private val senders = ArrayList<Pair<CancellableContinuation<Unit>, Int>>()
+    private val buffer = ArrayList<Int>()
+    private val receivers = ArrayList<CancellableContinuation<Int>>()
+
+    suspend fun send(x: Int) {
+        if (resumeFirstReceiver(x)) return
+        if (tryBufferElem(x)) return
+        suspendCancellableCoroutine { cont -> senders.add(cont to x) }
+    }
+
+    private fun tryBufferElem(elem: Int): Boolean {
+        if (buffer.size < capacity) {
+            buffer.add(elem)
+            return true
+        }
+        return false
+    }
+
+    private fun resumeFirstReceiver(elem: Int): Boolean {
+        while (receivers.isNotEmpty()) {
+            val r = receivers.removeFirst()
+            if (r.resume(elem)) return true
+        }
+        return false
+    }
+
+    suspend fun receive(): Int {
+        return getBufferedElem()
+            ?: resumeFirstSender()
+            ?: suspendCancellableCoroutine { cont -> receivers.add(cont) }
+    }
+
+    private fun getBufferedElem(): Int? {
+        val elem = buffer.removeFirstOrNull()?.also {
+            // The element is retrieved from the buffer, resume one sender and save its element in the buffer
+            resumeFirstSender()?.also { buffer.add(it) }
+        }
+        return elem
+    }
+
+    private fun resumeFirstSender(): Int? {
+        while (senders.isNotEmpty()) {
+            val (sender, elem) = senders.removeFirst()
+            if (sender.resume(Unit)) return elem
+        }
+        return null
+    }
+}
+
+class SequentialBuffered1Channel : SequentialBufferedChannel(1)
+class SequentialBuffered2Channel : SequentialBufferedChannel(2)
 
 @OptIn(InternalCoroutinesApi::class)
 private fun <T> CancellableContinuation<T>.resume(res: T): Boolean {
